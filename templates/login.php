779cd85bc9a4adc051a8491fc9eefe3ae038d880
Léa Maubert <leamaubert@hotmail.fr>
2023-07-26 18:03:55 +0200

    Dynamisation du forum, ajout formulaire creation de sujet

--- templates/login.php
+++ templates/login.php
@@ -7,7 +7,7 @@
             <p class="col-lg-10 fs-4">Vous trouverez ci-dessous un exemple de formulaire entièrement construit avec les contrôles de formulaire de Bootstrap. Chaque groupe de formulaires requis possède un état de validation qui peut être déclenché en cas de tentative d'envoi du formulaire sans l'avoir complété.</p>
         </div>
         <div class="col-md-10 mx-auto col-lg-5">
-            <form class="p-4 p-md-5 border rounded-3 bg-body-tertiary">
+            <form method="post" action="" class="p-4 p-md-5 border rounded-3 bg-body-tertiary">
                 <div class="form-floating mb-3">
                     <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
                     <label for="floatingInput">adresse e-mail</label>
